69af212af50514fb86121919be2a0588f749bf64
rashidsarkar <rashidrock558@gmail.com>
2023-09-13 03:12:14 +0600

    list update for only one

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,19 +3,26 @@ import SingleCard from "./SingleCart/SingleCard";
 import NameCard from "../NameCard/NameCard";
 
 const Cart = () => {
-  const [allActors, setAllActors] = useState([]);
+  const [allActorsData, setAllActorsData] = useState([]);
+  const [selectAcctor, setSelectAcctor] = useState([]);
   useEffect(() => {
     fetch("data.json")
       .then((res) => res.json())
-      .then((data) => setAllActors(data));
+      .then((data) => setAllActorsData(data));
   }, []);
   const selectActor = (acctor) => {
-    console.log(acctor);
+    // setSelectAcctor([...selectAcctor, acctor]);
+    const oldAcc = [...selectAcctor];
+    // console.log(selectAcctor);
+    if (!oldAcc.includes(acctor)) {
+      oldAcc.push(acctor);
+      setSelectAcctor(oldAcc);
+    }
   };
   return (
     <div className="flex justify-around ">
       <div className="flex justify-between w-[900px] flex-wrap gap-4  ">
-        {allActors.map((acctor, idx) => (
+        {allActorsData.map((acctor, idx) => (
           <SingleCard
             selectActor={selectActor}
             acctor={acctor}
@@ -24,7 +31,7 @@ const Cart = () => {
         ))}
       </div>
       <div className="bg-fuchsia-500 w-[300px]">
-        <NameCard></NameCard>
+        <NameCard selectAcctor={selectAcctor}></NameCard>
       </div>
     </div>
   );
--- src/components/NameCard/NameCard.jsx
+++ src/components/NameCard/NameCard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const NameCard = () => {
+const NameCard = ({ selectAcctor }) => {
   return (
     <div>
-      <div className="text-white cart">
-        <h2 className="text-4xl">this is price cart </h2>
+      <div className="py-5 text-white cart">
+        <h2 className="text-2xl text-center">this is price cart </h2>
+        <h3>Total Actor : {selectAcctor.length}</h3>
+        {selectAcctor.map((acctor, idx) => (
+          <li key={idx}>{acctor.name}</li>
+        ))}
       </div>
     </div>
   );
